abc909d2246ca8d5760367cdc755e1083207d28a
Candy-Tewari <sansshobhit@gmail.com>
2020-03-06 22:17:56 +0530

    First Commit

--- app/src/main/java/com/rocketapp/mediumos/MainActivity.java
+++ app/src/main/java/com/rocketapp/mediumos/MainActivity.java
@@ -1,33 +1,11 @@
 package com.rocketapp.mediumos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public void connect(View view){
-        ViewModel viewModel = new ViewModel();
-        EditText url = findViewById(R.id.url);
-        try {
-            System.out.println("================================="+url.getText().toString());
-            boolean access = viewModel.getAccess(url.getText().toString());
-            if(access){
-                Toast.makeText(this, "Connection Established!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, Navigator.class));
-            }
-            else{
-                Toast.makeText(this, "Connection cannot be established.", Toast.LENGTH_SHORT).show();
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
